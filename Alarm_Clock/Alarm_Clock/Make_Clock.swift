d9a476af7683c10a314360e8099a4e653522c599
stevewreese <stephenw.reese@gmail.com>
2018-01-25 21:46:10 -0700

    added time label

--- Alarm_Clock/Alarm_Clock/Make_Clock.swift
+++ Alarm_Clock/Alarm_Clock/Make_Clock.swift
@@ -12,11 +12,94 @@ class Make_Clock : UIView {
     
     private let clockLabel: UILabel = {
         let clockLabel = UILabel(frame: .zero)
-        clockLabel.text = "this will be the clock"
+        
         clockLabel.textColor = UIColor.green
+        clockLabel.adjustsFontSizeToFitWidth = true
+        clockLabel.textAlignment = NSTextAlignment.center
         return clockLabel
     }()
     
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        addSubview(clockLabel)
+    }
+    
+    var drawingBounds: CGRect {
+        if (bounds.width <= 0.0 || bounds.height <= 0.0)
+        {
+            return .zero
+        }
+        if(bounds.width > bounds.height){
+            
+            return CGRect(x: bounds.midX - (bounds.height * 0.5), y: 0.0, width: bounds.height, height: bounds.height)
+            
+        }else{
+            
+            return CGRect(x: 0.0, y: bounds.midY - (bounds.width * 0.5), width: bounds.width, height: bounds.width)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect){
+        guard let context: CGContext = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        context.clear(bounds)
+        context.setFillColor((backgroundColor ?? UIColor.white).cgColor)
+        context.fill(bounds)
+        context.setFillColor(UIColor.darkGray.cgColor)
+        clockLabel.frame = drawingBounds
+        let date = Date()
+        let calendar = Calendar.current
+        var hour = calendar.component(.hour, from: date)
+        var timeOfDay: String = ""
+        if(hour >= 12)
+        {
+            hour = hour - 12
+            if(hour == 0)
+            {
+                hour = 12
+            }
+            timeOfDay = "PM"
+        }
+        else{
+            timeOfDay = "AM"
+        }
+        let minutes = calendar.component(.minute, from: date)
+    
+        let day = calendar.component(.weekday, from: date)
+        let weekday = getDayofWeek(weekDay: day)
+        clockLabel.text = "\(weekday) -> \(hour):\(minutes) \(timeOfDay)"
+        
+        
+    }
+    
+    func getDayofWeek(weekDay: Int) -> String{
+        
+        switch(weekDay) {
+        case 1 :
+            return "Sunday"
+        case 2  :
+            return "Monday"
+        case 3 :
+            return "Tuesday"
+        case 4  :
+            return "Wednesday"
+        case 5 :
+            return "Thursday"
+        case 6  :
+            return "Friday"
+        case 7 :
+            return "Saturday"
+        default:
+            return ""
+        }
+        
+    }
+
     
     
     
